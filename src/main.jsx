cd3347b46055c2e32eaaca9f62faf17af61ed528
israDev <villar_80@hotmail.com>
2026-03-30 09:47:18 +0200

    feat: fix bugs for tailwind config, & upload tests Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Importamos el sistema de rutas
+import "./index.css";
+import App from "./App.jsx";
+import Prueba from "./Prueba.jsx"; // Importamos tu componente de prueba
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Ruta principal: http://localhost:5173/ */}
+        <Route path="/" element={<App />} />
+
+        {/* Tu página de prueba: http://localhost:5173/prueba */}
+        <Route path="/prueba" element={<Prueba />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
