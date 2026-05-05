4b6a4da44ea80d0cb8ea699551ee0ef52b0cd9fd
israDev <villar_80@hotmail.com>
2026-05-05 23:36:06 +0200

    feat: add delete & update to answerService

--- src/services/answerService.js
+++ src/services/answerService.js
@@ -10,6 +10,7 @@
 
 import { ENDPOINTS } from "../api/constantes";
 
+// DEvuelve las respuestas de un User
 export const getUserAnswers = async (skip = 0, limit = 20, token = null) => {
   const url = ENDPOINTS.USER_ANSWERS(skip, limit);
   console.log("Obteniendo respuestas de:", url);
@@ -32,4 +33,38 @@ export const getUserAnswers = async (skip = 0, limit = 20, token = null) => {
 
   const data = await response.json();
   return data;
+};
+
+// Elimina una Respuesta de un Usuario
+export const deleteAnswers = async (answerId, token) => {
+  const response = await fetch(ENDPOINTS.ANSWER_DETAIL(answerId), {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.detail || "Error al eliminar la respuesta");
+  }
+  return true;
+};
+
+// Actualiza una Respuesta de un Usuario
+export const updateAnswers = async (answerId, updatedAnswer, token) => {
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(updatedAnswer),
+  };
+  const response = await fetch(ENDPOINTS.ANSWER_DETAIL(answerId), options);
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.detail || "Error al actualizar la respuesta");
+  }
+  return true;
 };
