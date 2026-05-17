3e374bcb6681e7cb955752c60818a7cdcfbbe377
israDev <villar_80@hotmail.com>
2026-05-17 13:39:01 +0200

    fix: deploy err, editProfile import

--- src/components/profile/EditProfile.jsx
+++ src/components/profile/EditProfile.jsx
@@ -14,7 +14,7 @@ import { AuthContext } from "../../context/AuthContext";
 import { ENDPOINTS } from "../../api/constantes";
 
 import { avatarGroups } from "../../api/helpers";
-import AvatarPicker from "./AvatarPicker";
+import AvatarPicker from "./avatarPicker";
 
 import {
   HiOutlineUser,
